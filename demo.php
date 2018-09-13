f38eb8814f2febc522f856ee1429b038a613ad23
hdebiao <2504882007@qq.com>
2018-09-13 09:22:35 +0800

    update

--- demo.php
+++ demo.php
@@ -1,5 +1,89 @@
 <?php
-require_once  './vendor/autoload.php';
-use Hdb\Auth\Tools;
+require_once './vendor/autoload.php';
 
-Tools::Hello();
+use Hdb\Auth\Auth;
+
+header("Content-type: text/html; charset=utf-8");
+
+$dataConfig = array(
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => '1122',
+    'port' => 3306,
+    'database' => 'auth'
+);
+
+$action = array_key_exists('action', $_GET) ? $_GET['action'] : '';
+$uid = array_key_exists('uid', $_GET) ? $_GET['uid'] : 0;
+
+//if (empty($action) || empty($uid)) {
+//    echo '参数错误';
+//    exit();
+//}
+//switch ($action) {
+//
+//    case 'addRule' :
+//
+//        break;
+//
+//    case  'deleteRule' :
+//        break;
+//
+//    default :
+//        break;
+//}
+
+
+$auth = new Auth($dataConfig);
+
+
+$tt = $auth->checkRule('checkProject', 2);
+//print_r($tt);exit();
+if (true === $tt) {
+    echo '有权限';
+} else {
+    echo '没有权限';
+}
+
+
+function checkProject($uid, $projectId)
+{
+
+}
+
+function callProjectUser($uid, $projectId)
+{
+
+}
+
+
+function deleteRule($ruleId, $uid)
+{
+
+}
+
+
+function checkPostParams($data)
+{
+    $res = array();
+    $params = array(
+        'uid' => array('type' => 'int'),
+        'action' => array('type' => 'string'),
+        'name' => array('type' => 'string', 'length' => '255'),
+        'groups',
+        'status',
+        'rules',
+        'username'
+    );
+
+    foreach ($params as $k => $v) {
+
+    }
+
+}
+
+
+
+
+
+
--- src/Hdb/Auth/Auth.php
+++ src/Hdb/Auth/Auth.php
@@ -14,37 +14,102 @@ class Auth
 
     public function connectDatabase($config)
     {
-
+        $servername = $config['host'];
+        $username = $config['user'];
+        $password = $config['password'];
+        $dbname = $config['database'];
         if ($this->db === null) {
-            $this->db =  '';
+            try {
+                $_opts_values = array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+                $this->db = new \PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $_opts_values);
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+                return false;
+            }
         }
-        
         return $this->db;
     }
 
 
-    public function add()
+    public function getDb()
+    {
+        return $this->db;
+    }
+
+
+    public function addRule()
     {
 
     }
 
 
+    public function checkRule($ruleName, $uid)
+    {
+        $rule = self::getRule($ruleName);
+        if (empty($rule)) {
+            return false;
+        }
+        $user = self::getUser($uid);
+        if (empty($user)) {
+            return false;
+        }
+        $ruleId = $rule['id'];
+        $userGroup = $user['groups'];
+        $checkRuleArray = self::getRuleByGroupId(explode(',', $userGroup));
+        if (empty($checkRuleArray)) {
+            return false;
+        }
+        foreach ($checkRuleArray as $v) {
+            $ruleArr = explode(',', $v['rules']);
+            if (in_array($ruleId, $ruleArr)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    public function check()
+    public function deleteRule($ruleId, $uid)
     {
 
     }
 
-    public function delete()
+    public function getUser($uid)
     {
-
+        $sql = 'select * from auth_user where id=:uid';
+        $sth = $this->db->prepare($sql);
+        $sth->execute(array('uid' => $uid));
+        $data = $sth->fetchAll(\PDO::FETCH_ASSOC);
+        if (!empty($data)) {
+            $data = $data[0];
+        } else {
+            $data = array();
+        }
+        return $data;
     }
 
-    public function getUser()
+
+    public function getRule($name)
     {
-        
+        $sql = 'select * from auth_rule where name=:name';
+        $sth = $this->db->prepare($sql);
+        $sth->execute(array('name' => $name));
+        $data = $sth->fetchAll(\PDO::FETCH_ASSOC);
+        if (!empty($data)) {
+            $data = $data[0];
+        } else {
+            $data = array();
+        }
+        return $data;
     }
 
-
+    public function getRuleByGroupId($idArr)
+    {
+        $idArr = array_values(array_unique($idArr));
+        $ids = implode(',', $idArr);
+        $sth = $this->db->prepare('select * from auth_group where id in (' . $ids . ')');
+        $sth->execute();
+        $data = $sth->fetchAll(\PDO::FETCH_ASSOC);
+        return $data;
+    }
 
 }
